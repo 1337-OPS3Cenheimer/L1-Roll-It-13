53623512216681329d5e2ab55c760bda9d7696d7
1337-OPS3Cenheimer <cooperpuruz4264@masseyhigh.school.nz>
2026-02-20 10:45:36 +1300

    Working single rounds

--- C_04_One_Round_v2.py
+++ C_04_One_Round_v2.py
@@ -1,5 +1,4 @@
 import random
-from tkinter.constants import FIRST
 
 
 def initial_points(which_player):
@@ -18,6 +17,12 @@ def initial_points(which_player):
     print(f"{which_player} - Roll 1: {roll_one} \t| Roll 2: {roll_two} \t| Total: {total} ")
 
     return  total, double
+
+def make_statement(statement, decor):
+    # Adds emoji / additional characters to the start and end of headings
+
+    ends = decor * 3
+    print(f"\n{ends} {statement} {ends}")
 
 # Main starts here
 
@@ -72,7 +77,6 @@ while player_1_points < 13 and player_2_points < 13:
         break
 
     # Second person rolls the die (and score is updated)
-
     player_2_roll = random.randint(1, 6)
     player_2_points += player_2_roll
 
@@ -80,4 +84,30 @@ while player_1_points < 13 and player_2_points < 13:
 
     print(f"{first}: {player_1_points}  | {second}: {player_2_points}")
 
-print("End of round")
+# print("End of round")
+
+# Associate player points with either the user or the computer
+user_points = player_1_points
+comp_points = player_2_points
+
+# Switch the user and the computer points if the computer went first
+if first == "Computer":
+    user_points, comp_points = comp_points, user_points
+
+# Work out who won
+if user_points > comp_points:
+    winner = "User"
+else:
+    winner = "Computer"
+
+round_feedback = f"The {winner} won."
+
+# Double user points if eligible
+if winner == "User" and double_user == "yes":
+    user_points = user_points * 2
+
+# Output round results
+make_statement("Round Results", "=")
+print(f"User Points: {user_points} | Computer Points: {comp_points}")
+print(round_feedback)
+print()
