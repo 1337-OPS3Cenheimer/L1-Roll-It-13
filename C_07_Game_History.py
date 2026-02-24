2cc8ee40571f59fcfe2bdcc00abd13bdec6808c7
1337-OPS3Cenheimer <cooperpuruz4264@masseyhigh.school.nz>
2026-02-24 13:38:49 +1300

    Working game history component

---
+++ C_07_Game_History.py
@@ -0,0 +1,24 @@
+
+# Initialise list to hold game history
+game_history = []
+
+# Get data (base component does this already, code below for testing purposes)
+
+while True:
+    rounds_played = input("Round? ")
+    if rounds_played == "":
+        break
+    user_points = int(input("User points? "))
+    comp_points = int(input("Computer points?"))
+    winner = input("Who won? ")
+    user_score = int(input("User score: "))
+    comp_score = int(input("Computer score: "))
+
+    game_results = f"Round {rounds_played}: User Points: {user_points} | Computer Points {comp_points}, {winner} wins (15 | 0)"
+
+    game_history.append(game_results)
+
+print("Game History")
+
+for item in game_history:
+    print(item)
